849e7cf03df9fa2462bd5f2a9f43280cf30b84f7
jorginho84 <jp.rodriguez.osorio@gmail.com>
2020-08-19 13:14:32 -0400

    Create parameters.py
    Included parameters class. This class loads parameters.

---
+++ parameters.py
@@ -0,0 +1,13 @@
+
+"""
+Parameters class. Defines set of parameters
+"""
+
+class Parameters:
+    """
+
+	List of structural parameters and prices
+
+	"""
+    def __init__(self,alphas,betas,gammas,hw,porc,pro,pol):
+        self.alphas,self.betas,self.gammas,self.hw,self.porc,self.pro,self.pol= alphas,betas,gammas,hw,porc,pro,pol
