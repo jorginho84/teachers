fixed errors in moments computation

corr_simulate.py
@@ -98,7 +98,7 @@ def corr_simulate(data, B):
         est_corr_EXPPru[i] = corrM.iloc[3]['TEST']
         datav_2 = rev[rev['d_trat']==0]
         perc_inter_c[i] = (sum(datav_2['trame']==2) / len(datav_2['trame']))
-        perc_avanexpet_c[i] = (sum(datav_2['trame']==3) / (sum(datav_2['trame']==4)+sum(datav_2['trame']==5))) / len(datav_2['trame'])
+        perc_avanexpet_c[i] = ((sum(datav_2['trame']==3) + sum(datav_2['trame']==4)+sum(datav_2['trame']==5))) / len(datav_2['trame'])
         p1 = datav_2['score_port'].to_numpy()
         p2 = datav_2['score_test'].to_numpy()
         p1v1 = np.where(np.isnan(p1), 0, p1)
@@ -197,8 +197,7 @@ print(result)
 ##### PYTHON TO EXCEL #####
 
 wb = load_workbook('/Users/jorge-home/Dropbox/Research/teachers-reform/codes/teachers/Outcomes.xlsx')
-
-sheet = wb.active
+sheet = wb["data"]
 
 
 sheet['E5'] = result['Mean Portfolio']

estimate.py
@@ -138,7 +138,7 @@ class estimate:
             #datav0 = datadfT[datadfT['TREATMENT']==0]
             datav_2 = datadfT[datadfT['TREATMENT']==0]
             perc_inter_c[i] = np.mean((datav_2['PLACEMENT']==2))
-            perc_avanexpet_c[i] = np.mean((datav['PLACEMENT']==3) | (datav['PLACEMENT']==4)| (datav['PLACEMENT']==5))
+            perc_avanexpet_c[i] = np.mean((datav_2['PLACEMENT']==3) | (datav_2['PLACEMENT']==4)| (datav_2['PLACEMENT']==5))
             p1 = np.array(datav_2['PORTFOLIO'])
             p2 = np.array(datav_2['TEST'])
             p1v1 = np.where(np.isnan(p1), 0, p1)